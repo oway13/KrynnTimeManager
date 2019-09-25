2318d82a25f769214bb2cfcbfafe6773cb1e3d24
Oway13 <wyattcody133@gmail.com>
2019-09-25 11:09:17 -0500

    KrynnDateTime new Constructor for most params, start on Time Adding Methods

--- KrynnTimeManager/lib/KrynnDateTime.cs
+++ KrynnTimeManager/lib/KrynnDateTime.cs
@@ -18,9 +18,20 @@ namespace KrynnTimeManager.lib
             this.Hour = 0;
             this.Second = 0;
             this.DayOfWeek = this.Day % 7;
+
         }
 
-        public static readonly KrynnDateTime MinValue;
+        public KrynnDateTime(int year, int month, int day, int hour, int minute, int second)
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+            this.Minute = minute;
+            this.Hour = hour;
+            this.Second = second;
+            this.DayOfWeek = this.Day % 7;
+        }
+
         public int Year { get; }
         public int Month { get; }
         public int Minute { get; }
@@ -28,6 +39,15 @@ namespace KrynnTimeManager.lib
         public int Day { get; }
         public int Second { get; }
         public int DayOfWeek { get; }
+
+
+        //TODO: Add Month, Year Switching Behavior to something
+        public KrynnDateTime AddDays(int value)
+        {
+            return new KrynnDateTime(Year, Month, Day + value, Hour, Minute, Second);
+        }
+
+
 
         public string toDoubleDigit(int toConvert)
         {
